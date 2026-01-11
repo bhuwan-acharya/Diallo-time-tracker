f7a56a113ac9592e5a7a9255596b5e9beed1f5e5
Bhuwan Acharya <bhuwanacharya56@gmail.com>
2026-01-11 16:38:42 +0100

    test

--- time-tracker-backend/server.js
+++ time-tracker-backend/server.js
@@ -11,6 +11,7 @@ import { sendEmail } from './emailService.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 
